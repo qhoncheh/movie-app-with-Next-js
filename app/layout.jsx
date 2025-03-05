389ec68d21ffe0046fcf52eff8816a88ccf03c83
qhoncheh <ghoncheh.ataee@gmail.com>
2025-03-05 12:15:52 +0330

    ✔️

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+const RoutLayout = ({ children }) => {
+  return (
+    <html lang="fa-IR">
+      <body>{children}</body>
+    </html>
+  );
+}
+
+export default RoutLayout
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="bg-red-500 text-white">
+      <div className="p-8  m-20 bg-blue-600 w-96 h-20 flex font-bold text-center rounded-xl border-white-500 border-x-4">Home Lorem, ipsum dolor.</div>
+    </div>
+  );
+};
+
+export default HomePage;
